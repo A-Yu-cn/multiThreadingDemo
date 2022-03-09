01e0cea1f5d4f8f44ffaf3cd560cda562b4f77db
AYu <2426774691@qq.com>
2022-03-09 17:21:58 +0800

    learn to mutex

---
+++ Constructor/2.cpp
@@ -0,0 +1,33 @@
+#include <chrono>    // std::chrono::seconds ，时间库
+#include <iostream>  // std::cout
+#include <thread>    // std::thread, std::this_thread::sleep_for
+
+void thread_task(int n) {
+    std::this_thread::sleep_for(std::chrono::seconds(n));
+    std::cout << "hello thread "
+        << std::this_thread::get_id()
+        << " paused " << n << " seconds" << std::endl;
+}
+
+/*
+ * ===  FUNCTION  =========================================================
+ *         Name:  main
+ *  Description:  program entry routine.
+ * ========================================================================
+ */
+int main(int argc, const char *argv[])
+{
+    std::thread threads[5];
+    std::cout << "Spawning 5 threads...\n";
+    for (int i = 0; i < 5; i++) {
+        threads[i] = std::thread(thread_task, i + 1);
+    }
+    std::cout << "Done spawning threads! Now wait for them to join\n";
+    // std::cout << std::this_thread::get_id()<<std::endl; // 主线程的id为1，合理
+    for (auto& t: threads) {
+        t.join();
+    }
+    std::cout << "All threads joined.\n";
+
+    return EXIT_SUCCESS;
+}  /* ----------  end of function main  ---------- */
---
+++ hello/2.cpp
@@ -0,0 +1,7 @@
+#include "1.cpp"
+
+// 试一下多文件能不能运行，不过因为1.cpp也有main函数所以要把1.cpp的main改掉
+int main(){
+    thread_task();
+    return 0;
+}
